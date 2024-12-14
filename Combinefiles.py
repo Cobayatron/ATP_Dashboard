aee9283a7dfb310645dccbdbea0e85c4c3fec77c
Cobayatron <150964739+Cobayatron@users.noreply.github.com>
2024-12-14 19:53:39 +0000

    Modified to reflect new Data folder

--- Combinefiles.py
+++ Combinefiles.py
@@ -2,7 +2,7 @@ import pandas as pd
 def Combinefiles (year1,year2):
     anos_list=[]
     for i in range(year1,year2):
-        ano=pd.read_csv(f'../Data/atp_matches_{i}.csv')
+        ano=pd.read_csv(f'Data/atp_matches_{i}.csv')
         anos_list.append(ano)
     all=pd.concat(anos_list)    
     return(all)
